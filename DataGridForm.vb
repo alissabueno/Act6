22845effb1554610161f9212ee7fa7da47f6c952
alissabueno <94299513+alissabueno@users.noreply.github.com>
2023-05-02 20:49:38 +0800

    Add files via upload

---
+++ DataGridForm.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Data.Common
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports MySql.Data.MySqlClient
+Public Class DataGridForm
+    Dim connectionString As String = "Server=127.0.0.1;Database=trialdb;Uid=root;Pwd=alissamoresbueno;"
+    Dim connection As MySqlConnection = New MySqlConnection(connectionString)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query As String = "SELECT * FROM users"
+        Dim adapter As MySqlDataAdapter = New MySqlDataAdapter(query, connection)
+        Dim table As DataTable = New DataTable()
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BackupModule.BackupTableToExcel("MyTable", "C:\Users\User\OneDrive\Documents\template.xlsx", "C:\Users\User\Desktop\MyBackup.xlsx")
+    End Sub
+End Class
